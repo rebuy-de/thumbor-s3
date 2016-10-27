f59a6b3a26f204cb7a07001337989fd2acfd534e
Björn Häuser <b.haeuser@rebuy.de>
2016-10-27 14:03:53 +0200

    Update to thumbor >= 6
    Therefore we need to load the file asnychronous. Unfortunately there was
    no good library which does this properly. Therefore I went down the path
    of calculating the signature by myself.

--- setup.py
+++ setup.py
@@ -3,16 +3,21 @@ from distutils.core import setup
 
 setup(
     name='thumbor-s3',
-    version='0.1.1',
+    version='0.2.0',
     author='Björn Häuser',
-    author_email='b.haeuser@rebuy.de',
+    author_email='b.haeuser@rebuy.com',
     packages=['thumbor_s3'],
     url='https://github.com/rebuy-de/thumbor-s3',
     license='LICENSE',
     description='Load images from private S3 buckets for thumbor',
     long_description=open('README.txt').read(),
     install_requires=[
-        "boto >= 2.36.0",
-        "thumbor >= 4.11.1",
+        "tornado>=4.1.0,<5.0.0",
+        "thumbor>=6.1.5,<7",
         ],
+    extras_require={
+        'tests': [
+            "freezegun>=0.3.7,<1"
+        ],
+    },
     )
